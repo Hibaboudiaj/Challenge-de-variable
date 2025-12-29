7594989ce87264891447758907ecb36b9ad38f7a
Hibaboudiaj <boudiajhiba2@gmail.com>
2025-12-29 15:13:16 +0100

    Delete functions directory

--- functions/challenge12.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-let myarray = [1 , 2 , 5 , 5 , 7 , 8]
-
-function supprimerDoublons(myarray) {
-  return [...new Set(myarray)];
-}
-
-console.log(supprimerDoublons(myarray));
--- functions/challenge13.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-let myarray = ["hiba" , "imane" , "malaki" , "oussama"];
-function motsLongs(myarray, longueur) {
-  return myarray.filter(mot => mot.length >= longueur);
-}
-console.log(motsLongs(myarray, 5));
